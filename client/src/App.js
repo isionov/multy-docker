8694a04f0bedf756ddfd9554b948ac5b2fee05f8
Yana Ionova <yana.timofeeva.94@mail.ru>
2020-06-24 12:42:22 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Fib from "./Fib";
-import OtherPage from "./OtherPage";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Fib from './Fib';
+import OtherPage from './OtherPage';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/">Home</Link>
+          <Link to="/">Home Updated</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
